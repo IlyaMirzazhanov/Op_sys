7d9584a9ebf75af5264581ceaef340380dd64043
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-10-12 23:28:46 +0300

    Update ex2.c

--- week08/ex2.c
+++ week08/ex2.c
@@ -4,21 +4,18 @@
 //by Ilya Mirzazhanov BS20-06
 int main() {
     
-    //Task 2: From vmstat: free memory amount is decreasing, si and so is always 0  
-    //Task 3 VIRT and RES memory usage increases, %MEM increases
-
+    //Task 2: From vmstat: free memory lowering, si and so is always 0  
+    //Task 3 %MEM increases, VIRT and RES memory usage increases
     // vmstat 1 runned
     
-	int N = 10;                              
-	int mult = 10 * 1024 * 1024;                                     
-    for (int i = 0; i < N; i++) {                                   
-         int * p = (int*)malloc(mult);
-         memset(p, 0, mult); sleep(1);                               
+	int N = 10; int mul = 10 * 1024 * 1024;                                     
+    for (int i = 0; i<N; i++) {                                   
+         int * p = (int*)malloc(mul);
+         memset(p, 0, mul); sleep(1);                               
     }
 
     /*for (int i = 0; i < 10; ++i) {
-        calloc(sizeof(char), 1024 * 1024 * 10);
-        sleep(1);
+        calloc(sizeof(char), 1024 * 1024 * 10); sleep(1);
     }*/
 
     return 0;
