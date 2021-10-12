03f99d7c43dc57783afcb3eecd84697e80371255
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-10-12 23:53:10 +0300

    Update ex4.c

--- week08/ex4.c
+++ week08/ex4.c
@@ -1,16 +1,15 @@
 #include <stdlib.h>                                           
 #include <stdio.h>                                                                                  
-#include <sys/resource.h>                       
+#include <sys/resource.h>                                                          
 #include <unistd.h>                                                                   
 //by Ilya Mirzazhanov BS20-06
 int main() {                                        
-    int N = 10; int dt = 1024*1024*N;                    
-                                                                                             
-    for (int i = 0; i < N; i++) {                           
+    int N = 10; int dt = 1024*1024*N;                                                                                                           
+    for (int i=0; i<N; i++) {                           
        int *p = (int*) malloc(dt); memset(p, 0, dt );                                             
-        struct rusage memuse; getrusage(RUSAGE_SELF,&memuse);   //using getrusage                                                       
+        struct rusage memuse; getrusage(RUSAGE_SELF, &memuse);   //using getrusage                                                       
          printf(" MEMUSE: %ld\n", memuse.ru_maxrss );                              
-
+                                    
             sleep(1);   
      }                                      
      return 0;                                                       
