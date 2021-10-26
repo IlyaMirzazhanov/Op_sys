cd5a2e59e68870f0d88d93c6cb9286fc709bda0d
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-10-26 23:11:49 +0300

    Update ex4.c

--- week10/ex4.c
+++ week10/ex4.c
@@ -1,31 +1,31 @@
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h>                                          
 #include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/types.h>                                            
 //by Ilya Mirzazhanov BS20-06
 int main() {
      DIR *dir = opendir("tmp");
    
-    if(dir==NULL) {                                                                         
+     if(dir==NULL) {                                                                         
         return 1;                                       
     }
 
-    char tmp[512]; struct dirent *dp; struct stat ST; strcpy(tmp, "tmp/");
-    while((dp=readdir(dir))!=NULL) {                                        
+     char tmp[512]; struct dirent *dp; struct stat ST; strcpy(tmp, "tmp/");
+      while((dp=readdir(dir))!=NULL) {                                        
         stat(tmp, &ST); strcat(tmp, dp->d_name);
         if((strcmp(dp->d_name,".")!=0) & (strcmp(dp->d_name,"..")!=0) & (ST.st_nlink >= 2)) {
-            printf("link: %s files:\n", dp->d_name);
-            struct dirent *LINK; DIR *dir2 = opendir("tmp");                                    
-            while((LINK = readdir(dir2)) != NULL){
+                     printf("link: %s files:\n", dp->d_name);
+            struct dirent *LINK; DIR *dir2 = opendir("tmp");                                                  
+             while((LINK = readdir(dir2)) != NULL){
                 if((dp->d_ino == LINK->d_ino ) & (strcmp(LINK -> d_name, dp->d_name) != 0)) {                                
                     printf("%s ", LINK -> d_name); printf("\n");    
                 }
             }
                         printf("\n");                                       
         } else if((strcmp(dp->d_name,".")!=0)&(strcmp(dp->d_name,"..")!=0))                         
-                printf("file %s have no links\n", dp -> d_name);                            
+                 printf("file %s have no links\n", dp -> d_name);                            
 
                         strcpy(tmp, "tmp/");
     }
