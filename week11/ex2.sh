ee0dc8267a8778fa668017dc82267b37c325133b
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-11-02 23:49:18 +0300

    Update ex2.sh

--- week11/ex2.sh
+++ week11/ex2.sh
@@ -2,5 +2,7 @@
 sudo chmod 777 ./lofsdisk
 sudo echo 'Ilya' >> file1.txt
 sudo echo 'Mirzazhanov' >> file2.txt
-gcc ex2.c -o ex2
-sudo chroot ./lofsdisk
+gcc ex2.c -o ex2.out
+cp ex2.out lofsdisk
+sudo chroot lofsdisk /ex2.out > ex2.txt
+./ex2.out >> ex2.txt
