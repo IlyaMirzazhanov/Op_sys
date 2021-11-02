735dc6a73810f7c26e90f0f520e371339f9c31b7
IlyaMirzazhanov <52072483+IlyaMirzazhanov@users.noreply.github.com>
2021-11-02 23:52:08 +0300

    Update ex2.sh

--- week11/ex2.sh
+++ week11/ex2.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+mkdir -p lofsdisk/bin
+mkdir -p lofsdisk/lib64
+mkdir -p lofsdisk/usr/lib
+mkdir -p lofsdisk/usr/lib64
+cp /bin/bash lofsdisk/bin
+cp lofsdisk/usr/lib/* lofsdisk/lib
+cp lofsdisk/usr/lib64/* lofsdisk/lib64
 sudo chmod 777 ./lofsdisk
-sudo echo 'Ilya' >> file1.txt
-sudo echo 'Mirzazhanov' >> file2.txt
+echo 'Ilya' > lofsdisk/file1
+echo 'Mirzazhanov' > lofsdisk/file2
 gcc ex2.c -o ex2.out
 cp ex2.out lofsdisk
 sudo chroot lofsdisk /ex2.out > ex2.txt
 ./ex2.out >> ex2.txt
+
